6261dd5e96f30b0c2295774c5d27be4f858d3fc7
sofferjacob <sofferjacob@gmail.com>
2017-09-01 20:32:42 -0500

    Fix timer acurracy

--- kernel/timer.c
+++ kernel/timer.c
@@ -5,7 +5,7 @@
 
 void start_timer(float time, void(*listener)()) {
     size_t time_at_start = readSystemTime();
-    float total_time = (time * TICKSTOSEC) + time_at_start;  // Every 200 ticks is a second
+    float total_time = (time * (TICKSTOSEC / 2)) + time_at_start;  // Every 200 ticks is a second
     while (readSystemTime() < total_time) {
         // Wait for the times to match
     }
